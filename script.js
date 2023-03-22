e90b6af22aa2dc13b6d4f3a17c9c46a6309eb4b5
Govindsing Chavan <123160733+govind2611@users.noreply.github.com>
2023-03-22 10:34:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
 //your JS code here. If required.
+function submitForm() {
+  const ageInput = document.getElementById("age");
+  const nameInput = document.getElementById("name");
+  
+  if (ageInput.value.trim() === "" || nameInput.value.trim() === "") {
+    alert("Please fill in both age and name");
+    return;
+  }
+
+  const age = parseInt(ageInput.value.trim());
+  const name = nameInput.value.trim();
+  
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (age >= 18) {
+        resolve({ age, name });
+      } else {
+        reject({ age, name });
+      }
+    }, 4000);
+  });
+  
+  promise
+    .then(({ age, name }) => {
+      alert(`Welcome, ${name}. You can vote.`);
+      return age;
+    })
+    .then((age) => {
+      return { age };
+    })
+    .then((result) => {
+      alert(JSON.stringify(result));
+    })
+    .catch(({ age, name }) => {
+      alert(`Oh sorry ${name}. You aren't old enough.`);
+    });
+}
